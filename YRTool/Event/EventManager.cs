fd05291b94c8d5275c117e27dab081ad70cbfd3a
MBGS <1261482317@qq.com>
2023-06-20 19:46:05 +0800

    事件系统中文注释

--- YRTool/Event/EventManager.cs
+++ YRTool/Event/EventManager.cs
@@ -11,16 +11,16 @@ using UnityEngine;
 namespace YRTool
 {
 	/// <summary>
-	/// MMGameEvents are used throughout the game for general game events (game started, game ended, life lost, etc.)
+	/// 简易的事件示例
 	/// </summary>
-	public struct MMGameEvent
+	public struct SimpleGameEvent
 	{
 		public string EventName;
-		public MMGameEvent(string newName)
+		public SimpleGameEvent(string newName)
 		{
 			EventName = newName;
 		}
-		static MMGameEvent e;
+		static SimpleGameEvent e;
 		public static void Trigger(string newName)
 		{
 			e.EventName = newName;
@@ -64,29 +64,42 @@ namespace YRTool
 	///		}
 	/// } 
 	/// will catch all events of type MMGameEvent emitted from anywhere in the game, and do something if it's named GameOver
+	/// 事件系统 观察者模式
+	/// 首先，观察者类继承 EventListener<MMGameEvent> 并实现接口
+	/// 接着在观察者类中的激活函数和取消激活函数中登记/注销
+	/// void OnEnable()
+	/// {
+	/// 	this.MMEventStartListening<MMGameEvent>();
+	/// }
+	/// void OnDisable()
+	/// {
+	/// 	this.MMEventStopListening<MMGameEvent>();
+	/// }
+	/// 最后要触发某一类事件时，使用EventManager.TriggerEvent(new MMGameEvent())的形式来触发事件
+
 	/// </summary>
 	[ExecuteAlways]
 	public static class EventManager
 	{
-		private static Dictionary<Type, List<MMEventListenerBase>> _subscribersList;
+		private static Dictionary<Type, List<EventListenerBase>> _subscribersList;
 
 		static EventManager()
 		{
-			_subscribersList = new Dictionary<Type, List<MMEventListenerBase>>();
+			_subscribersList = new Dictionary<Type, List<EventListenerBase>>();
 		}
 
 		/// <summary>
-		/// Adds a new subscriber to a certain event.
+		/// 给特定事件添加一个监听者
 		/// </summary>
 		/// <param name="listener">listener.</param>
 		/// <typeparam name="MMEvent">The event type.</typeparam>
-		public static void AddListener<MMEvent>(MMEventListener<MMEvent> listener) where MMEvent : struct
+		public static void AddListener<MMEvent>(EventListener<MMEvent> listener) where MMEvent : struct
 		{
 			Type eventType = typeof(MMEvent);
 
 			if (!_subscribersList.ContainsKey(eventType))
 			{
-				_subscribersList[eventType] = new List<MMEventListenerBase>();
+				_subscribersList[eventType] = new List<EventListenerBase>();
 			}
 
 			if (!SubscriptionExists(eventType, listener))
@@ -96,11 +109,11 @@ namespace YRTool
 		}
 
 		/// <summary>
-		/// Removes a subscriber from a certain event.
+		/// 从特定事件中移除一个监听者
 		/// </summary>
 		/// <param name="listener">listener.</param>
 		/// <typeparam name="MMEvent">The event type.</typeparam>
-		public static void RemoveListener<MMEvent>(MMEventListener<MMEvent> listener) where MMEvent : struct
+		public static void RemoveListener<MMEvent>(EventListener<MMEvent> listener) where MMEvent : struct
 		{
 			Type eventType = typeof(MMEvent);
 
@@ -113,7 +126,7 @@ namespace YRTool
 #endif
 			}
 
-			List<MMEventListenerBase> subscriberList = _subscribersList[eventType];
+			List<EventListenerBase> subscriberList = _subscribersList[eventType];
 
 #if EVENTROUTER_THROWEXCEPTIONS
 	            bool listenerFound = false;
@@ -146,13 +159,13 @@ namespace YRTool
 		}
 
 		/// <summary>
-		/// Triggers an event. All instances that are subscribed to it will receive it (and will potentially act on it).
+		/// 触发一个特定事件，让所有的订阅者都触发
 		/// </summary>
-		/// <param name="newEvent">The event to trigger.</param>
-		/// <typeparam name="MMEvent">The 1st type parameter.</typeparam>
+		/// <param name="newEvent">触发事件数据</param>
+		/// <typeparam name="MMEvent">事件结构体</typeparam>
 		public static void TriggerEvent<MMEvent>(MMEvent newEvent) where MMEvent : struct
 		{
-			List<MMEventListenerBase> list;
+			List<EventListenerBase> list;
 			if (!_subscribersList.TryGetValue(typeof(MMEvent), out list))
 #if EVENTROUTER_REQUIRELISTENER
 			            throw new ArgumentException( string.Format( "Attempting to send event of type \"{0}\", but no listener for this type has been found. Make sure this.Subscribe<{0}>(EventRouter) has been called, or that all listeners to this event haven't been unsubscribed.", typeof( MMEvent ).ToString() ) );
@@ -162,19 +175,19 @@ namespace YRTool
 
 			for (int i = list.Count - 1; i >= 0; i--)
 			{
-				(list[i] as MMEventListener<MMEvent>).OnMMEvent(newEvent);
+				(list[i] as EventListener<MMEvent>).OnMMEvent(newEvent);
 			}
 		}
 
 		/// <summary>
-		/// Checks if there are subscribers for a certain type of events
+		/// 检查是否已经注册了一个监听者
 		/// </summary>
 		/// <returns><c>true</c>, if exists was subscriptioned, <c>false</c> otherwise.</returns>
 		/// <param name="type">Type.</param>
 		/// <param name="receiver">Receiver.</param>
-		private static bool SubscriptionExists(Type type, MMEventListenerBase receiver)
+		private static bool SubscriptionExists(Type type, EventListenerBase receiver)
 		{
-			List<MMEventListenerBase> receivers;
+			List<EventListenerBase> receivers;
 
 			if (!_subscribersList.TryGetValue(type, out receivers)) return false;
 
@@ -194,37 +207,43 @@ namespace YRTool
 	}
 
 	/// <summary>
-	/// Static class that allows any class to start or stop listening to events
+	/// 注册和注销事件监听
 	/// </summary>
 	public static class EventRegister
 	{
-		public delegate void Delegate<T>(T eventType);
+		//public delegate void Delegate<T>(T eventType);
 
-		public static void MMEventStartListening<EventType>(this MMEventListener<EventType> caller) where EventType : struct
+		public static void MMEventStartListening<EventType>(this EventListener<EventType> caller) where EventType : struct
 		{
 			EventManager.AddListener<EventType>(caller);
 		}
 
-		public static void MMEventStopListening<EventType>(this MMEventListener<EventType> caller) where EventType : struct
+		public static void MMEventStopListening<EventType>(this EventListener<EventType> caller) where EventType : struct
 		{
 			EventManager.RemoveListener<EventType>(caller);
 		}
 	}
 
 	/// <summary>
-	/// Event listener basic interface
+	/// 事件监听基础接口
 	/// </summary>
-	public interface MMEventListenerBase { };
+	public interface EventListenerBase { };
 
 	/// <summary>
-	/// A public interface you'll need to implement for each type of event you want to listen to.
+	/// 泛型的事件监听者，任意监听者都要实现这个接口
 	/// </summary>
-	public interface MMEventListener<T> : MMEventListenerBase
+	public interface EventListener<T> : EventListenerBase
 	{
 		void OnMMEvent(T eventType);
 	}
 
-	public class MMEventListenerWrapper<TOwner, TTarget, TEvent> : MMEventListener<TEvent>, IDisposable
+	/// <summary>
+	/// 封装的监听者类，MM给出的封装示例
+	/// </summary>
+	/// <typeparam name="TOwner"></typeparam>
+	/// <typeparam name="TTarget"></typeparam>
+	/// <typeparam name="TEvent"></typeparam>
+	public class MMEventListenerWrapper<TOwner, TTarget, TEvent> : EventListener<TEvent>, IDisposable
 		where TEvent : struct
 	{
 		private Action<TTarget> _callback;
