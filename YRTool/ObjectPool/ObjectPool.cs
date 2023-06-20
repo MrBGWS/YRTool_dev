b99505e908583bc0b57869d06c525d9be901cba0
MBGS <1261482317@qq.com>
2023-06-20 14:10:28 +0800

    对象池、事件系统雏形，搬运MM的代码

---
+++ YRTool/ObjectPool/ObjectPool.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace YRTool
+{
+	/// <summary>
+	/// 对象池
+	/// </summary>
+	public class ObjectPool : MonoBehaviour
+	{
+		public List<GameObject> PooledGameObjects;
+	}
+}
